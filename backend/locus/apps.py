454eb21ce34c5a52453dc0797029414ebba598d6
krishangpatney <krishangpatney@icloud.com>
2021-07-04 21:43:06 +0400

    removed backend files

--- backend/locus/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LocusConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'locus'
--- backend/locus/utilities.py
+++
@@ -1,3 +0,0 @@
-from geolib import geohash
-
-print(geohash.encode('70.2995', '-27.9993', 7))
